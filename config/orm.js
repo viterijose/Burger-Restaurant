a9451a11b74740790ea7955530abde5140ff815b
Jose Viteri <Jose@Joses-MacBook-Pro.local>
2018-05-24 18:23:44 -0400

    updated orm

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,15 @@
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+
 function objToSql(ob) {
     var arr = [];
 
