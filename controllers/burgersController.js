daaa2572b42ed129c57fff3fc7957841ee3a1ebf
Jose Viteri <Jose@Joses-MacBook-Pro.local>
2018-05-24 18:33:37 -0400

    updated burer controller

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -25,4 +25,13 @@ router.put("/api/burgers/:id", function (req, res) {
         }
     });
 });
+
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "name", "devoured"
+    ], [req.body.name, req.body.devoured],
+        function (result) {
+            res.json({ id: result.insertId });
+        })
+})
 module.exports = router;
