4ca34076d91342535124b758fd0a0fe8379e5b79
Jose Viteri <33139338+viterijose@users.noreply.github.com>
2018-05-27 20:43:05 -0400

    updated jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "CROCODILE1a",
-  database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "CROCODILE1a",
+    database: "burger_db"
+  });
+};
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
